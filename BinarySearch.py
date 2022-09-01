ae39a87b284df3927cb3f81ab298eedfb20b3d53
Hussein Lezzaik <hsenlezaik@gmail.com>
2022-08-31 20:28:16 -0400

    Create BinarySearch.py

---
+++ BinarySearch.py
@@ -0,0 +1,32 @@
+# Leetcode Problem 704
+
+# Iterative Solution: O(logn) time | O(1) space
+def binarySearch(array, target):
+    leftIdx = 0
+    rightIdx = len(array) - 1
+    while leftIdx <= rightIdx:
+        middleIdx = rightIdx - leftIdx // 2
+        potentialMatch = array[middleIdx]
+        if target == potentialMatch:
+            return middleIdx
+        elif target > potentialMatch:
+            leftIdx = middleIdx + 1
+        else:
+            rightIdx = middleIdx - 1
+    return -1
+  
+# Recursive Solution: O(logn) time | O(1) space
+def binarySearch(array, target):
+  return binarySearchHelper(array, target, 0, len(array) -1)
+
+def binarySearchHelper(array, target, left, right):
+    if left > right:
+        return - 1
+    middle = (left + right) // 2
+    potentialMatch = array[middle]
+    if target == potentialMatch:
+        return middle
+    elif target < potentialMatch:
+        return binarySearchHelper(array, target, left, middle -1)
+    else:
+        return binarySearchHelper(array, target, middle + 1, right)
